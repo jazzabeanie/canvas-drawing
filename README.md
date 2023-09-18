40454f030f61498141e660fba692fd891ea74b8a
Jared Johnston <jazzab@gmail.com>
2023-09-19 09:24:43 +1000

    Updates to sketch
    Update readme

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Canvas Drawing
 
 Some of my own play, and lessons from the creative coding domestika course.
+
+# Setup
+
+Run `npm install`
+
+# Usage
+
+The `html` files can simply be opened in the browser.
+
+`js` files are best run by the package `canvas-sketch`. Read about it here: https://github.com/mattdesl/canvas-sketch.
+
+Basic usage is to run `npx canvas-sketch-cli FILENAME`.
--- sketch-01.js
+++ sketch-01.js
@@ -9,8 +9,28 @@ const sketch = () => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    context.fillStyle = "black";
-    context.fillRect(10, 10, 10, 10);
+    const squares = Array.from(Array(5));
+
+    // const xOffset = 0.25;
+    // const yOffset = 0.25;
+    // const w = 0.5;
+    // const h = 0.5;
+    const w = Math.random();
+    const h = w;
+    const xOffset = (1 - w) / 2;
+    const yOffset = (1 - h) / 2;
+
+    squares.forEach((_, x) => {
+      squares.forEach((_, y) => {
+        context.fillStyle = "black";
+        context.fillRect(
+          (xOffset * width) / squares.length + (x * width) / squares.length,
+          (yOffset * width) / squares.length + (y * height) / squares.length,
+          (w * width) / squares.length,
+          (h * height) / squares.length,
+        );
+      });
+    });
   };
 };
 
