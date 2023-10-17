9ae11eae6547c1d949ea90c23387cae926f0c542
Jared Johnston <j.johnston@aims.gov.au>
2023-10-17 15:30:22 +1000

    Create a grid of rotated hexagons

---
+++ unit_2/hexagon_grid_rotation.js
@@ -0,0 +1,70 @@
+const canvasSketch = require('canvas-sketch');
+const pencil = require("../pencilCase.js")
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+const initRotation = 0.25 * Math.PI;
+var scale = 0
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    scale += 0.0001
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    context.fillStyle = "black";
+
+    const hexagon = (x, y, w, h) => {
+      const innerAngle = 120;
+      const externalTriangleAccuteAngle = 90 - (innerAngle/2)
+      const externalTriangleAccuteAngleRad = externalTriangleAccuteAngle*Math.PI/180
+      const xPoint = w/2;
+      const yPoint = xPoint * Math.tan(externalTriangleAccuteAngleRad)
+      const side = yPoint / Math.sin(externalTriangleAccuteAngleRad)
+      const shortSide = h - 2*yPoint
+      console.log(`side: ${side}`)
+
+      context.save()
+      context.translate(x, y);
+      context.translate(w/2, h/2); // get to the center
+
+      // TOO: implement rotation on a timer like grid-of-rotation
+      // context.rotate(initRotation + additionalRotation * scale);
+      context.rotate(initRotation + 2); 
+
+      context.beginPath();
+      context.moveTo(-w/2, -h/2+yPoint); // move to top left corner
+      context.lineTo(-w/2 + xPoint, -h/2);
+      context.lineTo(-w/2 + 2*xPoint, -h/2+yPoint);
+      context.lineTo(-w/2 + 2*xPoint, -h/2 + yPoint + shortSide);
+      context.lineTo(-w/2 + xPoint, -h/2 + h);
+      console.log(`bottom point = ${-h/2 + 2*yPoint + shortSide}`)
+      context.lineTo(-w/2, -h/2 + yPoint + shortSide);
+      context.lineTo(-w/2, -h/2+yPoint);
+      context.stroke();
+      context.restore()
+    }
+
+    const emptySquare = (x, y, w, h) => {
+      context.strokeRect(x, y, w, h);
+    }
+    const whole = new pencil.Space({
+      box: {
+        x: 0,
+        y: 0,
+        width: width,
+        height: height
+      }
+    });
+    // whole.create(hexagon)
+    const wholeGrid = whole.grid(9, 12)
+    console.log(`wholeGrid = ${JSON.stringify(wholeGrid)}`);
+    pencil.hexagonise(wholeGrid)
+    wholeGrid.forEach(square => square.create(hexagon))
+    // wholeGrid.forEach(square => square.create(emptySquare)) // for debuging purposes
+  };
+};
+
+canvasSketch(sketch, settings);
